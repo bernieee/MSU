c933a613647d535c186a8e0feb2b604d508b96c5
bernie <olga08_07@mail.ru>
2020-03-05 18:16:47 +0300

    made 1-10 problems

--- bgch/2_sem/hw13/hw13.h
+++ bgch/2_sem/hw13/hw13.h
@@ -9,12 +9,14 @@
 int init_array123(const char *fname, int n, double *x, double *y);
 int init_array4(const char *fname, int n, double *x, double *y, double *d);
 //double classic_newton(int n, double x0, double *x, double *y);
-int bisect_method(double a, double b, double eps, double *x, double (*func) (double x));//1
-int newton_method(double x0, double eps, double *x, double (*func) (double x), double (*deriv) (double x));//2
-int chords_method(double a, double b, double eps, double *x, double (*func) (double x));//3
-int secant_method(double a, double b, double eps, double *x, double (*func) (double x));//4
-int interpolation_2_method(double a, double b, double eps, double *x, double (*func) (double x));//5
-int interpolation_m_method(double a, double b, double eps, double *x, int m, double *d, double (*func) (double x));//6
-int approximation_m_method(double x0, double eps, double *x, double (*func) (double x));//7
-int linear_search_method(double a, double b, double eps, double *x, double (*func) (double x));//8
+int bisect_method_root(double a, double b, double eps, double *x, double (*func) (double x));//1
+int newton_method_root(double x0, double eps, double *x, double (*func) (double x), double (*deriv) (double x));//2
+int chords_method_root(double a, double b, double eps, double *x, double (*func) (double x));//3
+int secant_method_root(double a, double b, double eps, double *x, double (*func) (double x));//4
+int interpolation_2_method_root(double a, double b, double eps, double *x, double (*func) (double x));//5
+int interpolation_m_method_root(double a, double b, double eps, double *x, int m, double *d, double (*func) (double x));//6
+int approximation_m_method_root(double x0, double eps, double *x, double (*func) (double x));//7
+int linear_search_method_max(double a, double b, double eps, double *x, double (*func) (double x));//8
+int golden_section_method_max(double a, double b, double eps, double *x, double (*func) (double x));//9
+int interpolation_2_method_max(double a, double b, double eps, double *x, double (*func) (double x));//10
 
