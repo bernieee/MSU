618ad12bf2535279742b7f851bb3232215f09398
Kolomyttseva Olga <33371372+bernieee@users.noreply.github.com>
2020-05-29 22:39:08 +0300

    Bgch hw19 (#3)
    * hw19 done
    
    * hw17 hw18 added
    
    * hw17 hw18 changed for merge

---
+++ bgch/2_sem/hw19/10_hw19.c
@@ -0,0 +1,46 @@
+#include "hw19.h"
+
+
+int main(int argc, char *argv[])
+{
+    double a;
+    double b;
+    double t;
+    double res;
+    double eps;
+    int k;
+    int count;
+
+    count = 0;
+
+    if (argc != 4)
+    {
+        printf("Usage %s a eps k\n", argv[0]);
+        return -1;
+    }
+
+    sscanf(argv[1], "%lf", &a);
+    sscanf(argv[2], "%lf", &eps);
+    sscanf(argv[3], "%d", &k);
+
+    if (k < 0 || k > 6)
+    {
+        printf("Wrong k input\n");
+        return -1;
+    }
+
+    t = clock();
+    b = trapeze_area_eps_integral(a, eps, &res, k, &count, &func);
+    t = clock() - t;
+
+    if (!(fabs(b - ERROR) > 0))
+    {
+        printf("Answer = %10.3e  %d  %.2f\n", b, count, t / CLOCKS_PER_SEC);
+        return -1;
+    }
+
+    printf("Answer = %10.3e  %10.3e  %d  %.2f\n", res, b, count, t / CLOCKS_PER_SEC);
+
+    return 0;
+}
+
---
+++ bgch/2_sem/hw19/5_hw19.c
@@ -0,0 +1,42 @@
+#include "hw19.h"
+
+
+int main(int argc, char *argv[])
+{
+    double a;
+    double b;
+    double t;
+    double res;
+    int n;
+    int k;
+    int count;
+
+    count = 0;
+
+    if (argc != 5)
+    {
+        printf("Usage %s a b n k\n", argv[0]);
+        return -1;
+    }
+
+    sscanf(argv[1], "%lf", &a);
+    sscanf(argv[2], "%lf", &b);
+    n = atoi(argv[3]);
+    sscanf(argv[4], "%d", &k);
+
+
+    if (k < 0 || k > 6)
+    {
+        printf("Wrong k input\n");
+        return -1;
+    }
+
+    t = clock();
+    res = simpson_integral(a, b, n, k, &count, &func);
+    t = clock() - t;
+
+    printf("Answer = %10.3e  %d  %.2f\n", res, count, t / CLOCKS_PER_SEC);
+
+    return 0;
+}
+
