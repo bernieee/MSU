1cf8874b69dd67f6da74f64434056e8d40b3cd28
Kolomyttseva Olga <33371372+bernieee@users.noreply.github.com>
2020-11-01 21:45:25 +0300

    Bgch hw23 (#7)
    * student class sorts
    
    * added 1-3 problems, formula input & edited main input
    
    * edited 2 & merge
    
    * 2 done
    
    * done hw23
    
    * edited >< operators & edited 5, 8 tasks
    
    * edited read of list
    
    * edited hw22 from bgch_hw24
    
    * edited hw22 from bgch_hw24

---
+++ bgch/3_sem/hw23/5_hw23.cpp
@@ -0,0 +1,61 @@
+#include "solve_hw23.h"
+
+
+int main(int argc, char *argv[])
+{
+    char *fname = 0;
+    int max_print;
+    int size;
+    int ans;
+    time_t time;
+    list *head;
+
+    if (argc != 3 || atoi(argv[1]) <= 0)
+    {
+        printf("Usage %s max_print [file]\n", argv[0]);
+        return -1;
+    }
+
+    max_print = atoi(argv[1]);
+    fname = argv[2];
+
+    size = readList(fname, &head);
+
+    if (size < 0)
+    {
+        switch(size)
+        {
+            case student::OPEN_ERROR:
+                printf("Can not open file!\n");
+                break;
+            case student::MEMORY_ERROR:
+                printf("Can not allocate memory!\n");
+                break;
+            case student::READ_ERROR:
+                printf("Can not read an element!\n");
+                break;
+            default:
+                printf("Unknown error!\n");
+
+        }
+        return -1;
+    }
+
+    if (max_print > size)
+    {
+        max_print = size;
+    }
+
+    printList(head, max_print);
+
+    time = clock();
+    ans = maxlenUpSeqence(head);
+    time = clock() - time;
+
+    printf("Answer = %d\n", ans);
+
+    printf("Time = %lf\n", (double)time / CLOCKS_PER_SEC);
+
+    delList(head);
+    return 0;
+}
---
+++ bgch/3_sem/hw23/solve_hw23.h
@@ -0,0 +1,13 @@
+#include "list.h"
+#include "math.h"
+#include "time.h"
+
+
+int numOfMaxElems(list *head); //1
+int numOfElemsGreaterThanPrevious(list *head); //2
+int numberOfLocalMax(list *head); //3 not strictly
+int whatSequence(list *head); //4
+int maxlenUpSeqence(list *head); //5 strictly
+int numOfConstSeqence(list *head); //6 strictly
+int maxlenBetweenConstSeqence(list *head); //7 strictly
+int maxlenMonotonSeqence(list *head); //8 not strictly
